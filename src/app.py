d6cf54d78788029d2997435f2b472e5b2f1e3e57
LuisL <dev.eng.luisluna@gmail.com>
2024-07-17 20:57:24 +0000

    ejercicios terminados

--- src/app.py
+++ src/app.py
@@ -1,39 +1,40 @@
 from flask import Flask, jsonify, request, abort
+
 app = Flask(__name__)
 
-todos = [
+todos=[
     {"label": "My first task", "done": True},
     {"label": "My second task", "done": True},
     {"label": "My third task", "done": True}
 ]
 
 @app.route('/todos', methods=['GET'])
-def get_todos():
-    return jsonify(todos)
+def hello_world():
+    json_text = jsonify(todos)
+    return json_text
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
+    #Lineas de condicionales para verificar objetos
     request_body = request.json
-    print("Incoming request with the following body", request_body)
-    
-    # Agregar el nuevo todo a la lista
+    if not request_body:
+        abort(400, description="Request body must be JSON and contain 'label' and 'done' keys.")
+    if 'label' not in request_body or 'done' not in request_body:
+        abort(400, description="Request body must contain 'label' and 'done' keys.")
+    # Comando para agregar un objeto en la lista
     todos.append(request_body)
-    
-    # Devolver la lista actualizada de todos
-    return jsonify(todos), 201  # 201 Created es el código de estado HTTP adecuado
+    return jsonify(todos), 200
 
-@app.route("/todos/<int:position>", methods=['DELETE'])
+@app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
+    #Lineas de condicionales para verificar la ruta <int:position>
     if position < 0 or position >= len(todos):
-        # Si el índice está fuera del rango, devolver un error 404
-        abort(404, description="Todo not found")
+        abort(404, description="Todo with the given position not found.")
     
-    # Eliminar el todo del índice especificado
     print("This is the position to delete:", position)
-    del todos[position]
-    
-    # Devolver la lista actualizada de todos
-    return jsonify(todos), 200  # 200 OK es el código de estado HTTP adecuado
+    #borra de la lista de objetos segun la posicion: todos.pop(position)
+    deleted_todo = todos.pop(position)
+    return jsonify(todos), 200
 
-if __name__ == '__main__':
+if __name__=='__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
